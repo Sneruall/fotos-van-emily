f984a7fbbb399d1926db6b6de5fbfb1fe1d033b6
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-07-11 14:19:53 +0200

    Review section
    Added review text and first pill image

--- components/sections/reviews.js
+++ components/sections/reviews.js
@@ -1,4 +1,5 @@
 import Section from "../layout/Section";
+import Image from "next/image";
 
 function Reviews() {
   return (
@@ -8,6 +9,30 @@ function Reviews() {
           <h2 className="text-center md:text-left text-6xl font-display uppercase text-white leading-snug">
             Reviews
           </h2>
+          <div className="py-10 max-w-sm">
+            <p className="text-white text-center">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua. At vero eos et accusam et justo duo
+              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+              sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+              amet.
+            </p>
+            <p className="text-center mt-6 italic text-grey-dark">Name</p>
+          </div>
+          <div className="py-10 max-w-sm">
+            <Image
+              src="/images/tarieven.png"
+              width={342}
+              height={513}
+              alt="Geldboom"
+            />
+          </div>
+          <h2 className="text-2xl xl:text-3xl uppercase text-pink-darkest transition-all duration-100 ease-in-out group-hover:font-bold">
+            Tarieven
+          </h2>
+          <hr className="border-1 border-pink-darkest" />
+          <p className="mt-3 text-lg">Lorum ipsum</p>
         </div>
       </div>
     </Section>
