1130a083865381685d85bf53bf6873fcfc3ad594
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-07-11 07:39:10 +0200

    commented out section.js

--- components/sections/werkwijze.js
+++ components/sections/werkwijze.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import Section from "../layout/section";
+// import Section from "../layout/Section";
 
 
 export default function Werkwijze() {
   return (
-    <Section bg={"bg-blue-greyish"}>
+    // <Section bg={"bg-blue-greyish"}>
         <div className="bg-white max-w-4xl 2xl:max-w-5xl mx-auto p-5 pt-16 md:-mt-40 md:-mb-40 rounded-b-full rounded-t-full md:rounded-b-none">
           <p className="font-display text-brown text-center text-5xl mb-4 mt-0 md:mt-10 mx-0 md:mx-40">
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
@@ -57,6 +57,6 @@ export default function Werkwijze() {
             </div>
           </div>
         </div>
-    </Section>
+    // </Section>
   );
 }
