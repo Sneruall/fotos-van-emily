ee17bb12660ffc90afc4725eccbb844f15331703
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-07-16 10:43:51 +0200

    menu button mobile

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import NavItem from "./navItem";
+import { MenuIcon } from "@heroicons/react/solid";
+import Link from "next/link";
 
 function Header() {
   return (
-    <header className="flex flex-col my-10 h-screen">
+    <header className="flex flex-col h-screen">
       <Image
         src="/images/header/hero.png"
         alt="Hero image"
@@ -11,16 +13,24 @@ function Header() {
         objectFit="cover"
         priority
       />
-      <Image
-        className="object-contain"
-        src="/images/header/fotosvanemily-logo.png"
-        width={800}
-        height={140}
-        alt="Foto's van Emily logo"
-        priority
-      />
+
+      <div className="sm:hidden self-end z-50 mt-2 mr-2">
+          <MenuIcon className="h-10 w-10 text-grey-darkest" />
+      </div>
+
+      <div className="mx-10 mt-2 sm:mt-10 md:mx-auto">
+        <Image
+          className="object-contain"
+          src="/images/header/fotosvanemily-logo.png"
+          width={800}
+          height={140}
+          alt="Foto's van Emily logo"
+          priority
+        />
+      </div>
+
       <nav>
-        <ul className="flex flex-col md:flex-row container mx-auto m-5 justify-evenly max-w-3xl items-center relative">
+        <ul className="hidden sm:flex flex-row container mx-auto m-5 justify-evenly max-w-3xl items-center relative">
           <NavItem text={"Home"} />
           <NavItem text={"Diensten"} />
           <NavItem text={"Portfolio"} />
