a9043391bd212f031c0f61a222030e6743f4f384
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-07-21 10:56:32 +0200

    Update dropDownNavItemXS.js
    applied useState and useEffect for extended navbar functionality

--- components/layout/dropDownNavItemXS.js
+++ components/layout/dropDownNavItemXS.js
@@ -1,40 +1,52 @@
 import { ChevronUpIcon } from "@heroicons/react/outline";
 import { ChevronDownIcon } from "@heroicons/react/outline";
-import Link from "next/link";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import DroppedDownNavItemXS from "./droppedDownNavItemXS";
 
-
-
-function toggleDropdown(e) {
-  // Als het sub menu niet is uitgeklapt:
-  if (document.getElementById("diensten").classList.contains("hidden")){
-    document.getElementById("diensten").classList.toggle("hidden");
-    document.getElementById("chevrondown").classList.toggle("hidden");
-    document.getElementById("chevronup").classList.toggle("hidden");
-  }  // Als het sub menu wel is uitgeklapt:
-  else if (!document.getElementById("diensten").classList.contains("hidden")){
-    document.getElementById("diensten").classList.toggle("hidden");
-    document.getElementById("chevrondown").classList.toggle("hidden");
-    document.getElementById("chevronup").classList.toggle("hidden");
-  }
-}
-
 function dropDownNavItemXS(props) {
+  const [navExtended, setNavExtended] = useState(false);
+  let dienstenDropdown = null;
+
+  useEffect(() => {
+    if (!dienstenDropdown) {
+      dienstenDropdown = document.getElementById("diensten");
+    }
+
+    if (navExtended) {
+      dienstenDropdown.classList.remove("hidden");
+    } else if (!dienstenDropdown.classList.contains("hidden")) {
+      dienstenDropdown.classList.add("hidden");
+    }
+  }, [navExtended]);
+
+  function toggleDropdown() {
+    if (!navExtended) {
+      setNavExtended(true);
+    } else {
+      setNavExtended(false);
+    }
+  }
 
   return (
     <Fragment>
-        <li onClick={toggleDropdown} className="py-2 border-b border-opacity-25 cursor-pointer">
-          <div className="flex justify-between">
-            <div>
-              <h2 className="ml-12">{props.text}</h2>
-            </div>
-            <ChevronDownIcon id="chevrondown"
+      <li
+        onClick={toggleDropdown}
+        className="py-2 border-b border-opacity-25 cursor-pointer"
+      >
+        <div className="flex justify-between">
+          <div>
+            <h2 className="ml-12">{props.text}</h2>
+          </div>
+          {navExtended ? (
+            <ChevronUpIcon id="chevronup" className="h-6 w-6 my-auto mr-4" />
+          ) : (
+            <ChevronDownIcon
+              id="chevrondown"
               className="h-6 w-6 my-auto mr-4"
             />
-            <ChevronUpIcon id="chevronup" className="hidden h-6 w-6 my-auto mr-4" />
-          </div>
-        </li>
+          )}
+        </div>
+      </li>
       <ul id="diensten" className="hidden">
         <DroppedDownNavItemXS text="portret" link="/" />
         <DroppedDownNavItemXS text="portret" link="/" />
