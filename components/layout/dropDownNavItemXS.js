0cc5e0702a3090e5867d026cc1c0e60fc18cbfe1
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-07-21 08:56:01 +0200

    Update dropDownNavItemXS.js
    added toggle functionality sub menu diensten

--- components/layout/dropDownNavItemXS.js
+++ components/layout/dropDownNavItemXS.js
@@ -4,27 +4,37 @@ import Link from "next/link";
 import { Fragment } from "react";
 import DroppedDownNavItemXS from "./droppedDownNavItemXS";
 
-function toggleDropdown() {
-  document.getElementById("diensten").classList.replace("hidden", "visible");
+
+
+function toggleDropdown(e) {
+  // Als het sub menu niet is uitgeklapt:
+  if (document.getElementById("diensten").classList.contains("hidden")){
+    document.getElementById("diensten").classList.toggle("hidden");
+    document.getElementById("chevrondown").classList.toggle("hidden");
+    document.getElementById("chevronup").classList.toggle("hidden");
+  }  // Als het sub menu wel is uitgeklapt:
+  else if (!document.getElementById("diensten").classList.contains("hidden")){
+    document.getElementById("diensten").classList.toggle("hidden");
+    document.getElementById("chevrondown").classList.toggle("hidden");
+    document.getElementById("chevronup").classList.toggle("hidden");
+  }
 }
 
 function dropDownNavItemXS(props) {
+
   return (
     <Fragment>
-      <Link href="/">
-        <li className="py-2 border-b border-opacity-25 cursor-pointer">
+        <li onClick={toggleDropdown} className="py-2 border-b border-opacity-25 cursor-pointer">
           <div className="flex justify-between">
             <div>
               <h2 className="ml-12">{props.text}</h2>
             </div>
-            <ChevronDownIcon
-              onClick={toggleDropdown}
+            <ChevronDownIcon id="chevrondown"
               className="h-6 w-6 my-auto mr-4"
             />
-            <ChevronUpIcon className="hidden h-6 w-6 my-auto mr-4" />
+            <ChevronUpIcon id="chevronup" className="hidden h-6 w-6 my-auto mr-4" />
           </div>
         </li>
-      </Link>
       <ul id="diensten" className="hidden">
         <DroppedDownNavItemXS text="portret" link="/" />
         <DroppedDownNavItemXS text="portret" link="/" />
