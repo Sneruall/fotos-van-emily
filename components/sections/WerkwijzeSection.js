e90943d09b9e2c76e2fbceb7d427e00eab7d6d15
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-09-13 12:16:43 +0200

    werkwijze section mobile flexbox fix
    todo: fix height of collumns for desktop

--- components/sections/WerkwijzeSection.js
+++ components/sections/WerkwijzeSection.js
@@ -12,18 +12,41 @@ function WerkwijzeSection() {
         <div className="border border-white my-5">
           <div className="bg-white m-1 px-6 lg:px-16 py-4 bg-opacity-70">
             {/* Stap */}
-            <div className="grid grid-cols-8 lg:grid-cols-4 grid-rows-2">
-              <div className="col-span-1 row-span-1 lg:row-span-4 lg:ml-10 self-end">
+            <div className="flex flex-wrap lg:grid lg:grid-cols-4 lg:grid-rows-2">
+              <div className="lg:col-span-1 lg:row-span-2 lg:ml-10 lg:self-center">
                 <p className="font-display text-9xl text-white text-stroke-grey-warm text-stroke-2">
                   1
                 </p>
               </div>
-              <div className="lg:col-span-3 col-span-7 row-span-1 self-end mb-3 lg:mt-4 ml-4 lg:ml-0">
+              <div className="lg:col-span-3 lg:row-span-1 self-end mb-3 lg:mt-4 ml-4 lg:ml-0 flex-grow">
                 <h4 className=" text-brown-pale text-xl uppercase border-b-2 border-brown-pale2">
                   Contact vooraf
                 </h4>
               </div>
-              <div className="lg:col-start-2 col-span-8 lg:col-span-3">
+              <div className="lg:col-start-2 lg:col-span-3">
+                <p>
+                  Neem contact met mij op, dat kan via het contactformulier maar
+                  je mag me ook bellen of whatsappen. Ik laat zo snel mogelijk
+                  van mij horen, altijd binnen 24 uur. Samen bespreken we jouw
+                  wensen en vragen, we gaan op 
+                  
+                </p>
+              </div>
+            </div>
+            {/* Einde stap */}
+                     {/* Stap */}
+                     <div className="flex flex-wrap lg:grid lg:grid-cols-4 lg:grid-rows-2">
+              <div className="lg:col-span-1 lg:row-span-2 lg:ml-10 lg:self-center">
+                <p className="font-display text-9xl text-white text-stroke-grey-warm text-stroke-2">
+                  1
+                </p>
+              </div>
+              <div className="lg:col-span-3 lg:row-span-1 self-end mb-3 lg:mt-4 ml-4 lg:ml-0 flex-grow">
+                <h4 className=" text-brown-pale text-xl uppercase border-b-2 border-brown-pale2">
+                  Contact vooraf
+                </h4>
+              </div>
+              <div className="lg:col-start-2 lg:col-span-3">
                 <p>
                   Neem contact met mij op, dat kan via het contactformulier maar
                   je mag me ook bellen of whatsappen. Ik laat zo snel mogelijk
@@ -37,27 +60,25 @@ function WerkwijzeSection() {
               </div>
             </div>
             {/* Einde stap */}
-            {/* Stap */}
-            <div className="grid grid-cols-8 lg:grid-cols-4 grid-rows-2">
-              <div className="col-span-1 row-span-1 lg:row-span-4 lg:ml-10 self-end">
+                        {/* Stap */}
+                        <div className="flex flex-wrap lg:grid lg:grid-cols-4 lg:grid-rows-2">
+              <div className="lg:col-span-1 lg:row-span-2 lg:ml-10 lg:self-center">
                 <p className="font-display text-9xl text-white text-stroke-grey-warm text-stroke-2">
                   1
                 </p>
               </div>
-              <div className="lg:col-span-3 col-span-7 row-span-1 self-end mb-3 lg:mt-4 ml-4 lg:ml-0">
+              <div className="lg:col-span-3 lg:row-span-1 self-end mb-3 lg:mt-4 ml-4 lg:ml-0 flex-grow">
                 <h4 className=" text-brown-pale text-xl uppercase border-b-2 border-brown-pale2">
                   Contact vooraf
                 </h4>
               </div>
-              <div className="lg:col-start-2 col-span-8 lg:col-span-3 row-span-1">
+              <div className="lg:col-start-2 lg:col-span-3">
                 <p>
                   Neem contact met mij op, dat kan via het contactformulier maar
                   je mag me ook bellen of whatsappen. Ik laat zo snel mogelijk
                   van mij horen, altijd binnen 24 uur. Samen bespreken we jouw
-                  wensen en vragen, we gaan op zoek naar een locatie en we
-                  prikken een datum. Ik stuur een overzichtelijk mailtje met
-                  alle info/details die we besproken hebben en wat extra tips en
-                  adviezen voor de shoot.
+                  wensen en vragen, we gaan op 
+                  
                 </p>
               </div>
             </div>
