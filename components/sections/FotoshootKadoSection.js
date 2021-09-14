c3f5504e938b0971916911e0525e298ae9e16e56
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-09-14 11:03:05 +0200

    gift image

--- components/sections/FotoshootKadoSection.js
+++ components/sections/FotoshootKadoSection.js
@@ -1,11 +1,12 @@
 import Section from "../layout/Section";
 import Image from "next/image";
+import Button from "../ui/button";
 
 function FotoshootKadoSection() {
   return (
     <div className="relative">
-      <Section bg="bg-pink-200">
-        <div>
+      <Section bg="bg-pink-100">
+        <div className="">
           <Image
             src="/images/portretfotografie/gift.png"
             alt="portret"
@@ -14,7 +15,16 @@ function FotoshootKadoSection() {
             objectPosition="right"
           />
         </div>
-        <h1 className="title-1 z-50 relative">Een fotoshoot kado doen?</h1>
+
+        <div className="relative">
+          <h4 className="title-1-left">Een fotoshoot kado doen?</h4>
+          <h5 className="text-brown-pale text-3xl">
+            DAT KAN IN DE VORM VAN EEN CADEAUBON!
+          </h5>
+          <div className="my-10">
+            <Button link="/" button="button-1" text="Neem contact op" />
+          </div>
+        </div>
       </Section>
     </div>
   );
