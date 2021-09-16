97b9b301f3eb836e1f44320102b48da265138e88
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-09-16 14:44:22 +0200

    Revert "using headless UI for the dropdown diensten on index.js"
    This reverts commit 983d8e67451815a9f1cb4b06016ba301d07e4ad3.

--- components/layout/navItem.js
+++ components/layout/navItem.js
@@ -2,10 +2,24 @@ import Link from "next/link";
 
 function navItem(props) {
   return (
-    <li className="uppercase text-black text-center group w-44">
+    <li className="uppercase text-black text-center group w-32">
       <Link href={props.link}>
         <a className="hover:underline">{props.text}</a>
       </Link>
+      {props.dropdown && (
+        <ul>
+          <li className="hidden group-hover:block">
+            <Link href="/portretfotografie">
+              <a className="hover:underline">Portret</a>
+            </Link>
+          </li>
+          <li className="hidden group-hover:block">
+            <Link href="/loveshoot">
+              <a className="hover:underline">Loveshoot</a>
+            </Link>
+          </li>
+        </ul>
+      )}
     </li>
   );
 }
--- components/layout/navigation.js
+++ components/layout/navigation.js
@@ -1,49 +1,16 @@
 import NavItem from "./navItem";
-import { Menu } from "@headlessui/react";
 
 function navigation() {
   return (
-    <nav className="hidden font-sans sm:block max-w-6xl">
-      <ul className="flex flex-row justify-between mt-5 max-w-5xl items-start relative">
-        <NavItem text={"Home"} link="/" />
-        {/* Menu using headless UI */}
-        <div className="w-44">
-          <Menu>
-            <div className="flex flex-col">
-              <Menu.Button><span className="uppercase text-black hover:underline">Diensten</span></Menu.Button>
-              <Menu.Items>
-                <div className="flex flex-col">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <a
-                        className={`${active && "underline"} w-auto text-black text-center`}
-                        href="/portretfotografie"
-                      >
-                        Portret
-                      </a>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }) => (
-                      <a
-                        className={`${active && "underline"} w-auto text-black text-center`}
-                        href="/account-settings"
-                      >
-                        Loveshoot
-                      </a>
-                    )}
-                  </Menu.Item>
-                </div>
-              </Menu.Items>
-            </div>
-          </Menu>
-        </div>
-
-        <NavItem text={"Portfolio"} link="/portfolio" />
-        <NavItem text={"Over mij"} link="/over-mij" />
-        <NavItem text={"Contact"} link="/contact" />
-      </ul>
-    </nav>
+      <nav className="hidden font-sans sm:block max-w-6xl">
+        <ul className="flex flex-row justify-between mt-5 max-w-5xl items-start relative">
+          <NavItem text={"Home"} link="/" />
+          <NavItem text={"Diensten"} link="/" dropdown />
+          <NavItem text={"Portfolio"} link="/portfolio" />
+          <NavItem text={"Over mij"} link="/over-mij" />
+          <NavItem text={"Contact"} link="/contact" />
+        </ul>
+      </nav>
   );
 }
 
