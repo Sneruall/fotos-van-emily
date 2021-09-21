de0bba92b0df8c0c05ed988033b3d3a1713babf4
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-09-21 11:15:12 +0200

    Update scrollNavbar.js
    implemented the dropdown

--- components/layout/scrollNavbar.js
+++ components/layout/scrollNavbar.js
@@ -1,14 +1,15 @@
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 
 function scrollNavbar(props) {
   const [scrollNav, setScrollNav] = useState(false);
+  const [showDiensten, setShowDiensten] = useState(false);
 
   useEffect(() => {
     props.fade && window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
-    }
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
   const handleScroll = () => {
@@ -22,46 +23,86 @@ function scrollNavbar(props) {
   };
 
   return (
-    <div
-      className={`${
-        !scrollNav && props.fade ? "hidden" : "sm:block"
-      } hidden fixed bg-white opacity-90 w-full z-50 top-0 shadow-sm`}
-    >
-      <div className="px-2 flex max-w-6xl mx-auto uppercase text-brown-pale justify-between">
-        <div>
-          <h2 className="text-2xl md:text-4xl my-2 font-display">Foto's van Emily</h2>
-        </div>
-        <div className="my-auto">
-          <ul className="flex flex-row space-x-10">
-            <li>
+    <Fragment>
+      <div
+        className={`${
+          !scrollNav && props.fade ? "hidden" : "sm:block"
+        } hidden fixed bg-white w-full z-50 top-0 shadow-sm`}
+      >
+        <div className="px-2 flex max-w-6xl mx-auto uppercase text-brown-pale justify-between">
+          <div>
+            <h2 className="text-2xl md:text-4xl my-2 font-display">
+              Foto's van Emily
+            </h2>
+          </div>
+          <div className="flex items-stretch">
+            <ul className="grid grid-cols-5 justify-items-center pt-4 hover:cursor-pointer">
               <Link href="/">
-                <a className="hover:underline">Home</a>
+                <li className="w-32 text-center hover:underline">
+                  <a>Home</a>
+                </li>
               </Link>
-            </li>
-            <li className="group">
               <Link href="/">
-                <a className="hover:underline">Diensten</a>
+                <li
+                  onMouseEnter={() => setShowDiensten(true)}
+                  onMouseLeave={() => setShowDiensten(false)}
+                  className="w-32 text-center hover:underline"
+                >
+                  <a className="">Diensten</a>
+                </li>
               </Link>
-            </li>
-            <li>
               <Link href="/">
-                <a className="hover:underline">Portfolio</a>
+                <li className="w-32 text-center hover:underline">
+                  <a>Portfolio</a>
+                </li>
               </Link>
-            </li>
-            <li>
               <Link href="/">
-                <a className="hover:underline">Over mij</a>
+                <li className="w-32 text-center hover:underline">
+                  <a>Over mij</a>
+                </li>
               </Link>
-            </li>
-            <li>
               <Link href="/">
-                <a className="hover:underline">Contact</a>
+                <li className="w-32 text-center hover:underline">
+                  <a>Contact</a>
+                </li>
               </Link>
-            </li>
-          </ul>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+      {showDiensten && (
+        <div className="fixed w-full z-40 top-14">
+          <div className="w-full">
+            <div className="px-2 flex max-w-6xl mx-auto uppercase text-brown-pale justify-end h-full">
+              <ul className="grid grid-cols-5 grid-rows-3 justify-items-center">
+                <Link href="/">
+                  <li
+                    onMouseEnter={() => setShowDiensten(true)}
+                    onMouseLeave={() => setShowDiensten(false)}
+                    className="row-start-1 row-end-2 col-start-2 col-end-3 w-32 text-center shadow-sm h-10"
+                  >
+                    <div className="h-full bg-white pt-2 shadow-sm hover:underline hover:cursor-pointer">
+                      <a className="text-xs">Portret</a>
+                    </div>
+                  </li>
+                </Link>
+                <Link href="/">
+                  <li
+                    onMouseEnter={() => setShowDiensten(true)}
+                    onMouseLeave={() => setShowDiensten(false)}
+                    className="row-start-2 row-end-3 col-start-2 col-end-3 w-32 text-center shadow-sm"
+                  >
+                    <div className="h-full bg-white pt-2 shadow-sm hover:underline hover:cursor-pointer">
+                      <a className="text-xs">Zwangerschap</a>
+                    </div>
+                  </li>
+                </Link>
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
+    </Fragment>
   );
 }
 
