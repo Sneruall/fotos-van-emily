e2364b6dd6dcb5eb8e5cc25df937570e0cf79eea
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-09-23 19:27:40 +0200

    install react photo gallery and setup portfolio.js

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,8 +1,31 @@
 import { Fragment } from "react";
+import Image from "next/image";
 import ScrollNavbar from "../components/layout/ScrollNavbar";
 import Button from "../components/ui/button";
 
+import React, { useState, useCallback } from "react";
+// import { render } from "react-dom";
+import Gallery from "react-photo-gallery";
+// import Carousel, { Modal, ModalGateway } from "react-images";
+import { photos } from "./photos";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+
+
 function portfolio() {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
   return (
     <Fragment>
       <ScrollNavbar fade={false} />
@@ -14,6 +37,37 @@ function portfolio() {
           contact met mij op en we plannen samen een afspraak!
         </p>
         <Button link="/" button="button-1" text="Neem contact op" />
+      </div>
+      <div>
+        <Gallery photos={photos} onClick={openLightbox} direction="column" />
+        <Carousel>
+          <Image
+            src="/images/loveshoot/e1.png"
+            alt="Portretfoto"
+            width="300"
+            height="200"
+          />
+          <Image
+            src="/images/loveshoot/e1.png"
+            alt="Portretfoto"
+            width="300"
+            height="200"
+          />
+        </Carousel>
+        {/* <ModalGateway>
+          {viewerIsOpen ? (
+            <Modal onClose={closeLightbox}>
+              <Carousel
+                currentIndex={currentImage}
+                views={photos.map((x) => ({
+                  ...x,
+                  srcset: x.srcSet,
+                  caption: x.title,
+                }))}
+              />
+            </Modal>
+          ) : null}
+        </ModalGateway> */}
       </div>
     </Fragment>
   );
