18d6296800ca16fdaf73d3fa719d62e4302eb747
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-09-24 15:44:02 +0200

    Update portfolio.js

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -32,10 +32,10 @@ function portfolio() {
     console.log("set image" + index);
   }, []);
 
-//   const closeLightbox = () => {
-//     setCurrentImage(0);
-//     setShowModal(false);
-//   };
+  //   const closeLightbox = () => {
+  //     setCurrentImage(0);
+  //     setShowModal(false);
+  //   };
 
   return (
     <Fragment>
@@ -53,86 +53,60 @@ function portfolio() {
         <Gallery photos={photos} onClick={openLightbox} direction="column" />
 
         <>
-          <button
-            className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-            type="button"
-            onClick={() => setShowModal(true)}
-          >
-            Open regular modal
-          </button>
           {showModal ? (
             <>
-              <div className="justify-center items-center flex overflow-x-hidden overflow-y-hidden fixed inset-0 z-50 outline-none focus:outline-none">
-                <div className="relative mx-auto w-full h-full">
+              <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 max-h-screen">
+                <div className="relative mx-auto w-full h-screen">
                   {/*content*/}
-                  <div className="relative flex flex-col w-full h-full bg-black outline-none focus:outline-none">
+                  <div className="relative flex flex-col h-full bg-black">
                     {/*header*/}
-                    <div className="flex items-start justify-between p-5">
-                      <h3 className="text-3xl font-semibold">Modal Title</h3>
+                    <div className="absolute top-0 right-0 p-2 z-50">
                       <button
-                        className="p-1 ml-auto bg-transparent border-0 text-white float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                        className="p-1 ml-auto bg-transparent border-0 text-white float-right text-3xl leading-none font-semibold"
                         onClick={() => setShowModal(false)}
                       >
-                        <span className="bg-transparent text-white h-6 w-6 text-2xl block outline-none focus:outline-none">
+                        <span className="bg-transparent text-white h-6 w-6 text-4xl block">
                           ×
                         </span>
                       </button>
                     </div>
                     {/*body*/}
-                    <div className="relative p-6 flex-auto">
-                      <Carousel selectedItem={currentImage}>
-                        <Image
-                          src="/images/loveshoot/p1.png"
-                          alt="Portretfoto"
-                          layout="fill"
-                          objectFit="contain"
-                        />
-                        <Image
-                          src="/images/loveshoot/e1.png"
-                          alt="Portretfoto"
-                          width="1200"
-                          height="800"
-                        />
+                    <div className="p-6 h-full">
+                      <Carousel
+                        className="flex flex-col h-full justify-center"
+                        selectedItem={currentImage}
+                        showStatus={false}
+                        showIndicators={true}
+                        showArrows={true}
+                        showThumbs={false}
+                      >
+                        <div className="">
+                          <Image
+                            src="/images/loveshoot/p1.png"
+                            alt="Portretfoto"
+                            width={800}
+                            height={1200}
+                            objectFit="scale-down"
+                          />
+                        </div>
+
+                        <div className="">
+                          <Image
+                            src="/images/loveshoot/e1.png"
+                            alt="Portretfoto"
+                            width={1200}
+                            height={800}
+                          />
+                        </div>
                       </Carousel>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+              {/* <div className="opacity-25 fixed inset-0 z-40 bg-black"></div> */}
             </>
           ) : null}
         </>
-
-        <Carousel selectedItem="1">
-          <Image
-            src="/images/loveshoot/e1.png"
-            alt="Portretfoto"
-            width="300"
-            height="200"
-          />
-          <Image
-            src="/images/loveshoot/e2.png"
-            alt="Portretfoto"
-            width="300"
-            height="200"
-          />
-        </Carousel>
-
-     
-        {/* <ModalGateway>
-          {viewerIsOpen ? (
-            <Modal onClose={closeLightbox}>
-              <Carousel
-                currentIndex={currentImage}
-                views={photos.map((x) => ({
-                  ...x,
-                  srcset: x.srcSet,
-                  caption: x.title,
-                }))}
-              />
-            </Modal>
-          ) : null}
-        </ModalGateway> */}
       </div>
     </Fragment>
   );
