21543ceaf396fb746230c85cbb8b4868feb8d3a5
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-09-27 18:00:37 +0200

    Update ContactFormSection.js

--- components/sections/ContactFormSection.js
+++ components/sections/ContactFormSection.js
@@ -8,6 +8,9 @@ function ContactFormSection() {
       <div className="mx-auto max-w-6xl">
         {/* intro text */}
         <h1 className="title-1">Neem contact op</h1>
+        <p className="max-w-2xl mx-auto mb-10 text-center">
+        Wil je graag meer informatie, heb je vragen of wil je mij inhuren? Je kunt mij bereiken via onderstaand contactformulier of per mail, whatsapp of telefoon.
+        </p>
         <div className="flex flex-wrap gap-6 justify-center">
           <div className="flex gap-1 items-center">
             <MailIcon className="h-5 text-charcoal" />
