2fea710f6766bc19043dcfa40262ba5d2b66a9a7
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-09-28 09:31:10 +0200

    update fotoshootkadosection

--- components/sections/FotoshootKadoSection.js
+++ components/sections/FotoshootKadoSection.js
@@ -1,31 +1,26 @@
-import Section from "../layout/Section";
 import Image from "next/image";
 import Button from "../ui/button";
 
 function FotoshootKadoSection() {
   return (
     <div className="relative">
-      <Section bg="bg-pink-100">
-        <div className="hidden lg:block">
-          <Image
-            src="/images/portretfotografie/gift.png"
-            alt="portret"
-            layout="fill"
-            objectFit="contain"
-            objectPosition="right"
-          />
-        </div>
-
-        <div className="relative">
-          <h4 className="title-1-left">Een fotoshoot kado doen?</h4>
-          <h5 className="text-brown-pale text-3xl">
+      <Image
+        src="/images/portretfotografie/fotoshoot-cadeaubon-sectie.png"
+        alt="fotoshoot cadeaubon"
+        layout="fill"
+        objectFit="cover"
+      />
+      <div className="px-5 relative py-20 lg:py-72 max-w-6xl mx-auto">
+        <div className="max-w-sm">
+          <h4 className="title-1-left py-4 lg:pb-5">Een fotoshoot cadeau doen?</h4>
+          <h5 className="text-brown-pale text-2xl py-4 lg:pb-5 leading-7">
             DAT KAN IN DE VORM VAN EEN CADEAUBON!
           </h5>
-          <div className="my-10">
-            <Button link="/" button="button-1" text="Neem contact op" />
+          <div className="pb-10 pt-5">
+            <Button link="/" button="button-1" text="Meer informatie" />
           </div>
         </div>
-      </Section>
+      </div>
     </div>
   );
 }
