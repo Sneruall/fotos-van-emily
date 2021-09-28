99a6b0b469be1aea900b0ef20f7df41bc537f4e1
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-09-28 20:12:28 +0200

    Update scrollNavbar.js
    fix dropdown diensten

--- components/layout/scrollNavbar.js
+++ components/layout/scrollNavbar.js
@@ -26,7 +26,11 @@ function scrollNavbar(props) {
     <Fragment>
       <div
         className={`${
-          !scrollNav && props.fade ? `${props.lightFade ? "text-white" : "text-charcoal-2" } bg-opacity-0 shadow-none` : "bg-white text-brown-pale shadow-sm"
+          !scrollNav && props.fade
+            ? `${
+                props.lightFade ? "text-white" : "text-charcoal-2"
+              } bg-opacity-0 shadow-none`
+            : "bg-white text-brown-pale shadow-sm"
         } sm:block hidden fixed bg-white bg-opacity-90 w-full z-40 top-0`}
       >
         <div className="px-2 flex max-w-6xl mx-auto uppercase justify-between">
@@ -35,21 +39,92 @@ function scrollNavbar(props) {
               Foto's van Emily
             </h2>
           </div>
-          <div className="lg:flex sm:text-xs md:text-sm lg:text-sm hidden">
-            <ul className="flex justify-items-between gap-10 hover:cursor-pointer items-stretch">
+          <div className="lg:flex sm:text-xs md:text-sm hidden">
+            <ul className="flex justify-items-between gap-10 hover:cursor-pointer">
               <Link href="/">
                 <li className="text-center hover:underline flex flex-col justify-items-center">
                   <a className="my-auto">Home</a>
                 </li>
               </Link>
               <Link href="/#diensten">
-                <li
+                <div
                   onMouseEnter={() => setShowDiensten(true)}
                   onMouseLeave={() => setShowDiensten(false)}
                   className="text-center hover:underline flex flex-col justify-items-center"
                 >
                   <a className="my-auto">Diensten</a>
-                </li>
+                  {showDiensten && (
+                    <div className="fixed z-40 top-8 md:top-10 lg:top-12 lg:mt-1">
+                      <div className="w-full">
+                        <div className="flex max-w-6xl mx-auto uppercase text-brown-pale justify-end h-full">
+                          <ul className="flex flex-col justify-items-center text-left">
+                            <Link href="/portretfotografie">
+                              <li
+                                onMouseEnter={() => setShowDiensten(true)}
+                                onMouseLeave={() => setShowDiensten(false)}
+                                className="row-start-1 row-end-2 col-start-2 col-end-3 shadow-sm h-8 lg:h-10"
+                              >
+                                <div className="h-full bg-white bg-opacity-90 pt-1 lg:pt-2 shadow-sm hover:underline hover:cursor-pointer">
+                                  <a className="text-xxs lg:text-xs pl-4">Portret</a>
+                                </div>
+                              </li>
+                            </Link>
+                            <Link href="/loveshoot">
+                              <li
+                                onMouseEnter={() => setShowDiensten(true)}
+                                onMouseLeave={() => setShowDiensten(false)}
+                                className="row-start-2 row-end-3 col-start-2 col-end-3 shadow-sm h-8 lg:h-10"
+                              >
+                                <div className="h-full bg-white bg-opacity-90 pt-1 lg:pt-2 shadow-sm hover:underline hover:cursor-pointer">
+                                  <a className="text-xxs lg:text-xs pl-4">
+                                    Loveshoot
+                                  </a>
+                                </div>
+                              </li>
+                            </Link>
+                            <Link href="/zwangerschapsfotografie">
+                              <li
+                                onMouseEnter={() => setShowDiensten(true)}
+                                onMouseLeave={() => setShowDiensten(false)}
+                                className="row-start-3 row-end-4 col-start-2 col-end-3 shadow-sm h-8 lg:h-10"
+                              >
+                                <div className="h-full bg-white bg-opacity-90 pt-1 lg:pt-2 shadow-sm hover:underline hover:cursor-pointer">
+                                  <a className="text-xxs lg:text-xs pl-4">
+                                    Zwangerschap
+                                  </a>
+                                </div>
+                              </li>
+                            </Link>
+                            <Link href="/hondenfotografie">
+                              <li
+                                onMouseEnter={() => setShowDiensten(true)}
+                                onMouseLeave={() => setShowDiensten(false)}
+                                className="row-start-4 row-end-5 col-start-2 col-end-3 shadow-sm h-8 lg:h-10"
+                              >
+                                <div className="h-full bg-white bg-opacity-90 pt-1 lg:pt-2 shadow-sm hover:underline hover:cursor-pointer">
+                                  <a className="text-xxs lg:text-xs pl-4">Honden</a>
+                                </div>
+                              </li>
+                            </Link>
+                            <Link href="/">
+                              <li
+                                onMouseEnter={() => setShowDiensten(true)}
+                                onMouseLeave={() => setShowDiensten(false)}
+                                className="row-start-5 row-end-6 col-start-2 col-end-3 shadow-sm h-8 lg:h-10"
+                              >
+                                <div className="h-full pr-4 bg-white bg-opacity-90 pt-1 lg:pt-2 shadow-sm hover:underline hover:cursor-pointer">
+                                  <a className="text-xxs lg:text-xs pl-4">
+                                    Fotobewerking
+                                  </a>
+                                </div>
+                              </li>
+                            </Link>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </div>
               </Link>
               <Link href="/portfolio">
                 <li className="text-center hover:underline flex flex-col justify-items-center">
@@ -75,71 +150,6 @@ function scrollNavbar(props) {
           </div>
         </div>
       </div>
-      {showDiensten && (
-        <div className="fixed w-full z-40 top-8 md:top-10 lg:top-12 lg:mt-1">
-          <div className="w-full">
-            <div className="px-2 flex max-w-6xl mx-auto uppercase text-brown-pale justify-end h-full">
-              <ul className="grid grid-cols-6 grid-rows-5 justify-items-center">
-                <Link href="/portretfotografie">
-                  <li
-                    onMouseEnter={() => setShowDiensten(true)}
-                    onMouseLeave={() => setShowDiensten(false)}
-                    className="row-start-1 row-end-2 col-start-2 col-end-3 text-center shadow-sm h-8 lg:h-10"
-                  >
-                    <div className="h-full px-4 bg-white bg-opacity-90 pt-1 lg:pt-2 shadow-sm hover:underline hover:cursor-pointer">
-                      <a className="text-xxs lg:text-xs">Portret</a>
-                    </div>
-                  </li>
-                </Link>
-                <Link href="/loveshoot">
-                  <li
-                    onMouseEnter={() => setShowDiensten(true)}
-                    onMouseLeave={() => setShowDiensten(false)}
-                    className="row-start-2 row-end-3 col-start-2 col-end-3 text-center shadow-sm h-8 lg:h-10"
-                  >
-                    <div className="h-full bg-white bg-opacity-90 pt-1 lg:pt-2 shadow-sm hover:underline hover:cursor-pointer">
-                      <a className="text-xxs lg:text-xs">Loveshoot</a>
-                    </div>
-                  </li>
-                </Link>
-                <Link href="/zwangerschapsfotografie">
-                  <li
-                    onMouseEnter={() => setShowDiensten(true)}
-                    onMouseLeave={() => setShowDiensten(false)}
-                    className="row-start-3 row-end-4 col-start-2 col-end-3 text-center shadow-sm h-8 lg:h-10"
-                  >
-                    <div className="h-full bg-white bg-opacity-90 pt-1 lg:pt-2 shadow-sm hover:underline hover:cursor-pointer">
-                      <a className="text-xxs lg:text-xs">Zwangerschap</a>
-                    </div>
-                  </li>
-                </Link>
-                <Link href="/hondenfotografie">
-                  <li
-                    onMouseEnter={() => setShowDiensten(true)}
-                    onMouseLeave={() => setShowDiensten(false)}
-                    className="row-start-4 row-end-5 col-start-2 col-end-3 text-center shadow-sm h-8 lg:h-10"
-                  >
-                    <div className="h-full bg-white bg-opacity-90 pt-1 lg:pt-2 shadow-sm hover:underline hover:cursor-pointer">
-                      <a className="text-xxs lg:text-xs">Honden</a>
-                    </div>
-                  </li>
-                </Link>
-                <Link href="/">
-                  <li
-                    onMouseEnter={() => setShowDiensten(true)}
-                    onMouseLeave={() => setShowDiensten(false)}
-                    className="row-start-5 row-end-6 col-start-2 col-end-3 text-center shadow-sm h-8 lg:h-10"
-                  >
-                    <div className="h-full bg-white bg-opacity-90 pt-1 lg:pt-2 shadow-sm hover:underline hover:cursor-pointer">
-                      <a className="text-xxs lg:text-xs">Fotobewerking</a>
-                    </div>
-                  </li>
-                </Link>
-              </ul>
-            </div>
-        </div>
-        </div>
-      )}
     </Fragment>
   );
 }
