5ee4c9fb8484be4c125f60107f1ec966d549f640
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-09-29 17:56:08 +0200

    Update index.js
    new header inline with the design no component but custom for index

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ import Werkwijze from "../components/sections/werkwijze";
 import Reviews from "../components/sections/reviews";
 import ScrollNavbar from "../components/layout/ScrollNavbar";
 import Header from "../components/layout/header";
+import Image from "next/image";
+
 
 export default function Home() {
   return (
@@ -18,16 +20,44 @@ export default function Home() {
         {/* gebruik next-seo npm tool */}
       </Head>
 
-      {/* <ScrollNavbar2 fade /> */}
       <ScrollNavbar fade hideLogo />
 
-      <Header
-        img="/images/header/header1.png"
-        imgMobile="/images/header/header1m.png"
-        title="Foto's van Emily"
-        // integratedNavigation
-        // pageTitle2="FOTOGRAFIE &amp; FOTOBEWERKING"
-      />
+      {/* HEADER INDEX */}
+      <header className="relative">
+        <div className="hidden sm:block -mt-28 lg:-mt-14">
+          <Image
+            src="/images/header/header1.png"
+            alt="Foto's van Emily"
+            layout="fill"
+            objectFit="cover"
+            priority
+            quality={100}
+          />
+
+          <div className="relative mt-14 font-display uppercase text-charcoal mx-auto min-h-screen">
+            <div className="absolute top-20 left-0 right-0">
+              <h1 className="text-5xl md:text-8xl lg:text-9xl xl:text-10xl text-center">
+              Foto's van Emily
+              </h1>
+            </div>
+          </div>
+        </div>
+
+        <div className="sm:hidden -mt-14">
+          <Image
+            src="/images/header/header1m.png"
+            alt="Foto's van Emily"
+            layout="fill"
+            objectFit="cover"
+            priority
+          />
+            <div className="relative z-10 font-display uppercase text-charcoal mx-auto min-h-screen">
+              <div className="absolute top-40 left-0 right-0">
+                <h1 className="text-5xl text-center">Foto's van Emily</h1>
+              </div>
+            </div>
+        </div>
+      </header>
 
       <Welcome />
       <Diensten />
