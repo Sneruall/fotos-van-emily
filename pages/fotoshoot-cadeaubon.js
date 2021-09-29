e412caf8ea6e5d5a5c7d7ce91344133d13e482ef
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-09-29 11:34:48 +0200

    Update fotoshoot-cadeaubon.js

--- pages/fotoshoot-cadeaubon.js
+++ pages/fotoshoot-cadeaubon.js
@@ -2,6 +2,9 @@ import { Fragment } from "react";
 import Header from "../components/layout/header";
 import ScrollNavbar from "../components/layout/ScrollNavbar";
 import Section from "../components/layout/Section";
+import Image from "next/image";
+import { MailIcon, PhoneIcon } from "@heroicons/react/solid";
+import ContactFormElement from "../components/sections/elements/ContactFormElement";
 
 function fotoshootCadeaubon() {
   return (
@@ -14,8 +17,71 @@ function fotoshootCadeaubon() {
       />
 
       <Section bg={"bg-grey-light"}>
-        <div className="mx-auto max-w-6xl">
-          <h2 className="title-1 leading-tight">Doe een fotoshoot cadeau</h2>
+        <div className="mx-auto max-w-6xl px-4">
+          <div className="border-white border relative z-10">
+            <div className="bg-white bg-opacity-70 p-10 m-1">
+              <div className="mx-auto">
+                {/* Cadeaukaart */}
+                <h1 className="title-2 text-3xl mb-2 text-center">
+                  OP ZOEK NAAR EEN ORIGNEEL CADEAU?
+                </h1>
+                <div className="border-b-2 border-brown-pale2 w-full max-w-xs mx-auto"></div>
+                <h2 className="title-1 my-5 text-6xl leading-tight text-center ">
+                  Doe een fotoshoot cadeau!
+                </h2>
+                <div className="flex flex-col lg:flex-row gap-10 my-16 max-w-4xl mx-auto">
+                  <div>
+                    <Image
+                      src="/images/cadeaubon/Business-A4-Mockup3.png"
+                      alt="Emily van Roomen"
+                      width={1800}
+                      height={1628}
+                    />
+                  </div>
+                  <div>
+                    <p>
+                      Je beste vriendin verassen met een zwangerschapsshoot? Of
+                      je partner met een loveshoot? Verras ze met een fotoshoot!
+                      Een uniek en waardevol cadeau; niet alleen geef je ze een
+                      leuke ervaring maar ook een cadeau waar ze voor altijd van
+                      kunnen blijven genieten!
+                    </p>
+                    <p className="mt-5">
+                      Je kunt een specifieke shoot cadeau doen zoals een
+                      zwangerschap of portret. Maar je kunt ook een bepaald
+                      bedrag cadeau doen. De bon komt in de vorm van een mooie
+                      kaart die je zelf kunt invullen en personaliseren!
+                    </p>
+                  </div>
+                </div>
+                {/* Vraag een cadeaubon aan */}
+                <div className="my-10">
+                  <h1 className="title-2 text-3xl mb-2 text-center">
+                    Vraag een Cadeaubon aan
+                  </h1>
+                  <div className="border-b-2 border-brown-pale2 w-full max-w-xs mx-auto"></div>
+                </div>
+                <div className="flex flex-wrap gap-6 justify-center">
+                  <div className="flex gap-1 items-center">
+                    <MailIcon className="h-5 text-charcoal" />
+                    <a
+                      className="hover:underline"
+                      href="mailto:info@fotosvanemily.nl"
+                    >
+                      info@fotosvanemily.nl
+                    </a>
+                  </div>
+                  <div className="flex gap-1 items-center">
+                    <PhoneIcon className="h-5 text-charcoal" />
+                    <a className="hover:underline" href="tel:0031652331778">
+                      06 52 33 17 78
+                    </a>
+                  </div>
+                </div>
+                <ContactFormElement />
+              </div>
+            </div>
+          </div>
         </div>
       </Section>
     </Fragment>
