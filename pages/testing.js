9cfdf2c712a9ba5df8f6b956c2e68df553e8d31a
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-10-01 09:43:13 +0200

    Update testing.js
    full page color change working in testpage

--- pages/testing.js
+++ pages/testing.js
@@ -5,21 +5,37 @@ function testing() {
   return (
     <Fragment>
       <Controller>
-        <div className="h-screen bg-blue-900">
-          <h1 className="text-10xl">Dit is een titel</h1>
-        </div>
-        <Scene triggerElement="#spotlight" classToggle={("#spotlight", "bg-red-900")} indicators>
-          <div id="spotlight" className="h-screen bg-blue-900 transition duration-500 ease-in-out">
-            <h1 className="text-10xl text-black">Dit is een titel</h1>
+        <div
+          id="page"
+          className="bg-blue-900 transition duration-500 ease-in-out"
+        >
+          <div className="h-screen bg-blue-900">
+            <h1 className="text-10xl">Dit is een titel 1</h1>
           </div>
-        </Scene>
-        <Scene classToggle="bg-indigo-500">
-          <div className="h-screen bg-red-900 transition duration-500 ease-in-out">
-            <h1 className="text-10xl">Dit is een titel</h1>
+          <Scene
+            triggerElement="#spotlight"
+            classToggle={["#page", "bg-red-900"]}
+            indicators
+          >
+            <div>
+              <div id="spotlight" className="">
+                <div className="h-screen">
+                  <h1 className="text-10xl text-black">Dit is een titel 2a</h1>
+                </div>
+                <div className="h-screen">
+                  <h1 className="text-10xl text-black">Dit is een titel 2b</h1>
+                </div>
+              </div>
+            </div>
+          </Scene>
+          <Scene indicators classToggle={["#page", "bg-red-300"]}>
+            <div className="h-screen ">
+              <h1 className="text-10xl">Dit is een titel 3</h1>
+            </div>
+          </Scene>
+          <div className="h-screen bg-red-900">
+            <h1 className="text-10xl">Dit is een titel 4</h1>
           </div>
-        </Scene>
-        <div className="h-screen bg-red-900">
-          <h1 className="text-10xl">Dit is een titel</h1>
         </div>
       </Controller>
     </Fragment>
