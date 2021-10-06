b78ccb7485c5d397cf8b7166610ec1923b785d58
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-10-06 20:07:59 +0200

    added stars index

--- components/sections/welcome.js
+++ components/sections/welcome.js
@@ -11,7 +11,7 @@ export default function Welcome() {
         <div className="w-3/5 self-start mx-auto md:w-auto relative">
           <Image src="/images/emily.png" alt="Emily" width={370} height={553} quality={100} />
 
-          <div className="absolute top-0 right-0 md:left-0 transform translate-x-14 md:-translate-x-10 2xl:-translate-x-20 -translate-y-5">
+          <div className="absolute md:hidden top-0 right-0 transform translate-x-14 -translate-y-5">
             <Image
               src="/images/stars1.png"
               alt="Stars"
@@ -33,9 +33,22 @@ export default function Welcome() {
               hi! mijn naam is
             </p>
 
+            <div className="relative">
             <h2 className="font-display text-charcoal md:text-clay text-center md:text-left text-5xl md:text-7xl uppercase">
               Emily van Roomen
             </h2>
+            <div className="hidden md:block absolute top-0 right-0 transform translate-x-10 -translate-y-10 lg:-translate-x-28 lg:-translate-y-16 xl:translate-x-2">
+            <Image
+              src="/images/stars1.png"
+              alt="Stars"
+              width={81}
+              height={93}
+            />
+          </div>
+            </div>
+
+
+
 
             <p className="font-sans normal-case text-black text-center md:text-left mx-6 md:mx-0 mt-7">
             Ik fotografeer in de omgeving Zeist en Utrecht en ben gespecialiseerd in buitenfotografie. Ik werk graag in de buitenlucht omdat het mij in staat stelt oprechte, bijzondere foto’s te maken met een magisch tintje. Buiten voel je je meer op je gemak (in vergelijking tot een studio), hierdoor kun je beter jezelf zijn wat zorgt voor oprechte foto’s waar jij jezelf in herkent. De natuur en stedelijke gebieden zitten vol kleur en prachtige plekjes wat zorgt voor de mooiste achtergronden, zo krijg jij bijzondere en unieke foto’s! Daarnaast heeft de buitenlucht iets magisch, helemaal wanneer er zonnestralen prachtig op je vallen of de mist van de ochtend nog aanwezig is. Het lijkt mij leuk om foto’s van jou te maken, kijk rond door mijn portfolio of je mijn stijl leuk vindt en stuur gerust een berichtje als je vragen hebt. 
