08b84b81351a32db04cedebd009eb4131a579ae2
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-10-30 18:53:45 +0200

    Update bericht-verzonden.js
    fix too much padding mobile

--- pages/bericht-verzonden.js
+++ pages/bericht-verzonden.js
@@ -10,11 +10,11 @@ function berichtVerzonden() {
       <NextSeo noindex={true} />
       <ScrollNavbar fade={true} />
       <div className="h-screen bg-grey-light2 mx-auto text-center my-auto flex items-center -mt-14 lg:mt-0">
-        <div className="max-w-6xl px-10 lg:px-0 mx-auto">
+        <div className="max-w-6xl lg:px-0 mx-auto">
           <h1 className="text-5xl lg:text-8xl text-brown-pale mb-5 uppercase">
             Bericht verzonden!
           </h1>
-          <p className="text-charcoal text-xl mb-5">
+          <p className="text-charcoal text-xl mb-5 mx-5">
             Bedankt voor je bericht! Ik neem zo snel mogelijk contact met je op.
           </p>
           <Button link="/" button="button-1" text="terug naar home" />
